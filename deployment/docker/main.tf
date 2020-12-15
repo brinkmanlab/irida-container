c528b8749d05a563893c029b70ad814735a5285f
Nolan Woods <nolan_w@sfu.ca>
2020-12-15 12:22:00 -0800

    use default host port for galaxy

--- deployment/docker/main.tf
+++ deployment/docker/main.tf
@@ -13,7 +13,6 @@ module "galaxy" {
   admin_users = [var.email]
   email       = var.email
   debug       = var.debug
-  host_port = var.host_port
   extra_mounts = [
     {
       source = docker_volume.user_data.name
